fbcb2566c6365eef30396b872d6d6542954f8410
Margo <margo@Margos-MBP.home>
2023-03-15 14:21:28 +0000

    ProductCard

---
+++ src/components/buttons/AddToFavoriteButton/AddToFavoriteButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const AddToFavoriteButton: React.FC = () => {
+  return (
+    <button
+      type="button"
+      className="product-card__button-add-to-favorites"
+      //  onClick={}
+    >
+      Add to card
+    </button>
+  );
+};
