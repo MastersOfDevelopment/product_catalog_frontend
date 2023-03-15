aceee489e1cea709019fb0aa4a2d53c6e36bc134
Yurii Demkiv <demkiv123@gmail.com>
2023-03-16 00:00:37 +0200

    Added pageNotFound images and plugins

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 120,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  cssEnable: [
+    "css",
+    "less",
+    "sass"
+  ]
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 // import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/global.scss'
+import { PageNotFound } from './components/pages/PageNotFound'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.tsx
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <PageNotFound />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
