fa097d6da4c1578209c49fdb60d0af438bf4a87d
Ihor Mariash <imariash86@gmail.com>
2023-03-15 16:27:07 +0200

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
@@ -22,7 +23,8 @@ function App() {
         >
           Learn React!
         </a>
-      </header>
+      </header> */}
+      <Footer />
     </div>
   );
 }
