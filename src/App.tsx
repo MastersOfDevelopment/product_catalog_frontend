08781ed69d102169cf6dbcff9f4ca568001a880b
Ihor Mariash <imariash86@gmail.com>
2023-03-15 10:54:16 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Learn ReactJS!
         </a>
       </header>
     </div>
