94129f83912aea98a763423408d32801be649409
Ihor Mariash <imariash86@gmail.com>
2023-03-16 15:42:56 +0200

    modules for styles and logo were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/global.scss'
 import { PageNotFound } from 'components/pages/PageNotFound'
 
-import { Header } from './components/Header/Header'
+import { Header } from 'components/Header/Header'
 import { Footer } from './components/Footer'
 import { ProductCard } from './components/ProductCard'
 
