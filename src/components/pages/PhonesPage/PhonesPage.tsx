f6e70d61aa58b199155e9d74ca1a18bbd48cabe5
Yurii Demkiv <demkiv123@gmail.com>
2023-03-17 11:03:12 +0200

    added api and fetch request

--- src/components/pages/PhonesPage/PhonesPage.tsx
+++ src/components/pages/PhonesPage/PhonesPage.tsx
@@ -1,8 +1,30 @@
+import { getAllPhones } from 'api/getPhones'
 import { ProductCard } from 'components/ProductCard'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Phone } from 'types/Phone'
 
-export const PhonesPage: React.FC = () => (
-  <div className="container">
-    <ProductCard />
-  </div>
-)
+export const PhonesPage: React.FC = () => {
+  const [phones, setPhones] = useState<Phone[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  console.log(phones)
+
+  const fetchAllPhones = async () => {
+    try {
+      const phonesFromServer = await getAllPhones()
+
+      setIsLoading(true)
+      setPhones(phonesFromServer)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchAllPhones()
+  }, [])
+
+  return <div className="container">{phones.length > 0 && <ProductCard />}</div>
+}
