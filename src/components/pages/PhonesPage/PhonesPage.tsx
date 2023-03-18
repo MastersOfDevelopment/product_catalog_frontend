9774c13ae9f0a6bd2a0b2055713d6742fa514e1d
Margo <margo@Margos-MBP.home>
2023-03-18 07:45:36 +0000

    phonePage-layout

--- src/components/pages/PhonesPage/PhonesPage.tsx
+++ src/components/pages/PhonesPage/PhonesPage.tsx
@@ -2,6 +2,9 @@ import { getAllPhones } from 'api/getPhones'
 import { ProductCard } from 'components/ProductCard'
 import React, { useEffect, useState } from 'react'
 import { Phone } from 'types/Phone'
+import styles from './PhonesPage.module.scss'
+// import '../../../styles/grid.scss'
+import { Grid } from '@mui/material'
 
 export const PhonesPage: React.FC = () => {
   const [phones, setPhones] = useState<Phone[]>([])
@@ -26,5 +29,21 @@ export const PhonesPage: React.FC = () => {
     fetchAllPhones()
   }, [])
 
-  return <div className="container">{phones.length > 0 && <ProductCard />}</div>
+  return (
+    <div className={styles.grid}>
+      <div className={styles.main}>
+        <h1 className={styles.title}>Mobile phones</h1>
+        <p className={styles.subtitle}>95 models</p>
+        <div className={styles.phonePage}>
+          <Grid container justifyContent="center" rowSpacing={5} columnSpacing={2} className={styles.gridMui}>
+            {phones.map((phone) => (
+              <Grid key={1} justifyContent="center" item>
+                <ProductCard key={phone.phoneId} phone={phone} />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      </div>
+    </div>
+  )
 }
