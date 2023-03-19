6bdcac684b121c56414c2fdc9b971138c56056f0
Ihor Mariash <imariash86@gmail.com>
2023-03-19 12:28:10 +0200

    get products and show on the phonePage at start

--- src/components/pages/PhonesPage/PhonesPage.tsx
+++ src/components/pages/PhonesPage/PhonesPage.tsx
@@ -1,8 +1,31 @@
+import { Pagination } from 'components/Pagination'
 import { ProductCard } from 'components/ProductCard'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAll } from '../../../utils/getDataFromServer'
+import { ProductCardPhone } from '../../../types/ProductCardPhoneType'
+import '../../../styles/grid.scss'
 
-export const PhonesPage: React.FC = () => (
-  <div className="container">
-    <ProductCard />
-  </div>
-)
+export const PhonesPage: React.FC = () => {
+  const [allProducts, setAllProducts] = useState<ProductCardPhone[]>([])
+
+  const getAllProducts = async () => {
+    const data = await getAll()
+    setAllProducts(data)
+  }
+  useEffect(() => {
+    getAllProducts()
+  }, [])
+
+  console.log(allProducts)
+
+  return (
+    <>
+      <div className="container grid">
+        {allProducts.map(() => (
+          <ProductCard />
+        ))}
+      </div>
+      <Pagination />
+    </>
+  )
+}
