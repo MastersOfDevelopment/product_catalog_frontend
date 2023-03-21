c1811261c1c29695f6b0c0f0a73d93c8f898d7c4
Ihor Mariash <imariash86@gmail.com>
2023-03-21 22:33:19 +0200

    categories component was added to the homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable import/no-unresolved */
 import { BigSlider } from 'components/BigSlider'
+import { Categories } from 'components/Categories'
 import React from 'react'
 
 export const HomePage: React.FC = () => (
   <>
     <h1>Welcome to Nice Gadgets store!</h1>
     <BigSlider />
+    <Categories />
   </>
 )
