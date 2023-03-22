2752762edf6b12c8b90a7c793d53f34ee69cee1e
Margo <margo@Margos-MBP.home>
2023-03-22 13:15:33 +0000

    about, tech spec, back were added

--- src/pages/PhoneItem/PhoneItem.tsx
+++ src/pages/PhoneItem/PhoneItem.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable */
 import { memo, useCallback, useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { PhoneDetails } from 'types/PhoneDetails'
 import { getPhoneDetails, getAllPhones } from 'api/getPhones'
 import { useParams } from 'react-router-dom'
@@ -18,6 +17,8 @@ export const PhoneItem: React.FC = () => {
   const [mainPhoto, setMainPhoto] = useState<string | undefined>()
   const [currentPhoneId, setCurrentPhoneId] = useState<string>(phoneId)
   const [favoritePhone, setFavoritePhone] = useState<Phone | undefined>()
+
+  const navigate = useNavigate()
 
   const fetchOnePhone = useCallback(async (phoneId: string) => {
     try {
@@ -74,6 +75,13 @@ export const PhoneItem: React.FC = () => {
 
   return (
     <>
+      <div className={styles.history}>
+        <a href="/phones" className={styles.historyLink} />
+        <div className={styles.historyIcon} />
+        <button className={styles.historyTitle} onClick={() => navigate(-1)}>
+          Back
+        </button>
+      </div>
       <main>
         <h1 className={styles.title}>{`${phoneItem?.name}`}</h1>
         <section className={styles.characteristics}>
@@ -136,7 +144,8 @@ export const PhoneItem: React.FC = () => {
                 <AddToCardButton phoneId={phoneId} />
               </div>
               <div className={styles.butToFav}>
-              <AddToFavoriteButton phone={favoritePhone} /></div>
+                <AddToFavoriteButton phone={favoritePhone} />
+              </div>
             </div>
             <div className={styles.description}>
               <div className={styles.line}>
@@ -161,36 +170,39 @@ export const PhoneItem: React.FC = () => {
             </div>
           </div>
         </section>
-        <div>
-          <div>
+        <div className={styles.addInfo}>
+          <div className={styles.category}>
             <h2 className={styles.subTitle}>About</h2>
-            <div className={styles.separator} />
+            <div className={styles.separatorInfo} />
             <h3 className={styles.subSubTitle}>And then there was Pro</h3>
-            <p className={styles.textP}>A transformative triple‑camera system that adds tons of capability without
-              complexity.
+            <p className={styles.textP}>
+              A transformative triple‑camera system that adds tons of capability without complexity.
               <br />
               <br />
-              An unprecedented leap in battery life. And a mind‑blowing chip that
-              doubles down on machine learning and pushes the boundariesof what a
-              smartphone can do. Welcome to the first iPhone powerful enough to be
-              called Pro.
+              An unprecedented leap in battery life. And a mind‑blowing chip that doubles down on machine learning and
+              pushes the boundariesof what a smartphone can do. Welcome to the first iPhone powerful enough to be called
+              Pro.
             </p>
             <h3 className={styles.subSubTitle}>Camera</h3>
-            <p className={styles.textP}>Meet the first triple‑camera system to combine cutting‑edge technology
-              with the legendary simplicity of iPhone. Capture up to four times more
-              scene. Get beautiful images in drastically lower light. Shoot the
-              highest‑quality video in a smartphone — then edit with the same tools you
-              love for photos. You’ve never shot with anything like it.</p>
-            <h3 className={styles.subSubTitle}>Shoot it. Flip it. Zoom it. Crop it. Cut it. Light it. Tweak it. Love it.</h3>
-            <p className={styles.textP}>iPhone 11 Pro lets you capture videos that are beautifully true to life,
-              with greater detail and smoother motion. Epic processing power means it
-              can shoot 4K video with extended dynamic range and cinematic video
-              stabilization — all at 60 fps. You get more creative control, too, with
-              four times more scene and powerful new editing tools to play with.</p>
+            <p className={styles.textP}>
+              Meet the first triple‑camera system to combine cutting‑edge technology with the legendary simplicity of
+              iPhone. Capture up to four times more scene. Get beautiful images in drastically lower light. Shoot the
+              highest‑quality video in a smartphone — then edit with the same tools you love for photos. You’ve never
+              shot with anything like it.
+            </p>
+            <h3 className={styles.subSubTitle}>
+              Shoot it. Flip it. Zoom it. Crop it. Cut it. Light it. Tweak it. Love it.
+            </h3>
+            <p className={styles.textP}>
+              iPhone 11 Pro lets you capture videos that are beautifully true to life, with greater detail and smoother
+              motion. Epic processing power means it can shoot 4K video with extended dynamic range and cinematic video
+              stabilization — all at 60 fps. You get more creative control, too, with four times more scene and powerful
+              new editing tools to play with.
+            </p>
           </div>
-          <div>
+          <div className={styles.category}>
             <h2 className={styles.subTitle}>Tech specs</h2>
-            <div className={styles.separator} />
+            <div className={styles.separatorInfo} />
             <div>
               <div className={styles.description}>
                 <div className={styles.line}>
@@ -206,11 +218,6 @@ export const PhoneItem: React.FC = () => {
                 <div className={styles.line}>
                   <p className={styles.descriptionTitle}>Processor</p>
                   <p className={styles.descriptionData}>{phoneItem?.processor}</p>
-                </div>
-
-                <div className={styles.line}>
-                  <p className={styles.descriptionTitle}>RAM</p>
-                  <p className={styles.descriptionData}>{phoneItem?.ram}</p>
                 </div>
 
                 <div className={styles.line}>
@@ -235,7 +242,7 @@ export const PhoneItem: React.FC = () => {
 
                 <div className={styles.line}>
                   <p className={styles.descriptionTitle}>Cell</p>
-                  <p className={styles.descriptionData}>{phoneItem?.cell.join(',')}</p>
+                  <p className={styles.descriptionData}>{phoneItem?.cell.join(', ')}</p>
                 </div>
               </div>
             </div>
