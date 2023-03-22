ec9250d29e3b6b62d23087f42275c19436fdfcae
Iryna Pysanko <malva37@gmail.com>
2023-03-22 15:44:47 +0200

    added new endpoints

--- src/api/getPhones.ts
+++ src/api/getPhones.ts
@@ -1,3 +1,4 @@
+import { Phone } from 'types/Phone'
 import { PhoneDetails } from 'types/PhoneDetails'
 import { PhonesData } from 'types/PhonesData'
 import { client } from 'utils/fetch'
@@ -6,3 +7,5 @@ export const getPhoneDetails = async (phoneId: string) => client.get<PhoneDetail
 export const getAllPhones = async (searchParams: string) => {
   return client.get<PhonesData>(`/products?${searchParams}`)
 }
+export const getNewProducts = async () => client.get<Phone>('products/new')
+export const getRecommendedPhones = async (phoneId: string) => client.get<Phone>(`/phones/${phoneId}/recommended`)
