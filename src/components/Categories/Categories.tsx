345a4ee4f011b2ffa410a1071aa012941cc02185
Ihor Mariash <imariash86@gmail.com>
2023-03-22 20:33:15 +0200

    fixes

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,11 +1,27 @@
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router'
+import { getAllPhones } from 'api/getPhones'
 import styles from './Categories.module.scss'
 import phones from '../../assets/img/categories/category-phones.jpg'
 import tablets from '../../assets/img/categories/category-tablets.jpg'
 import accessories from '../../assets/img/categories/category-accessories.jpg'
 
 export const Categories: FC = () => {
+  const [totalPhones, setTotalPhones] = useState(0)
+
+  const location = useLocation()
+  const searchParams = new URLSearchParams(location.search).toString()
+
+  useEffect(() => {
+    const loadPhonesData = async () => {
+      const phonesData = await getAllPhones(searchParams)
+      setTotalPhones(phonesData.total)
+    }
+
+    loadPhonesData()
+  }, [searchParams])
+
   return (
     <div className={styles.categories}>
       <h2 className={styles.categories__title}>Shop by category</h2>
@@ -18,7 +34,7 @@ export const Categories: FC = () => {
           <Link to={'/phones'} className={styles.category__name}>
             Mobile phones
           </Link>
-          <p className={styles.category__itemsCount}>95 models</p>
+          <p className={styles.category__itemsCount}>{`${totalPhones} models`}</p>
         </li>
 
         <li className={styles.category}>
@@ -28,7 +44,7 @@ export const Categories: FC = () => {
           <Link to={'/tablets'} className={styles.category__name}>
             Tablets
           </Link>
-          <p className={styles.category__itemsCount}>24 models</p>
+          <p className={styles.category__itemsCount}>0 models</p>
         </li>
 
         <li className={styles.category}>
@@ -38,7 +54,7 @@ export const Categories: FC = () => {
           <Link to={'/accessories'} className={styles.category__name}>
             Accessories
           </Link>
-          <p className={styles.category__itemsCount}>100 models</p>
+          <p className={styles.category__itemsCount}>0 models</p>
         </li>
       </ul>
     </div>
